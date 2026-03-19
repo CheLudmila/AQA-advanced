688f9eb63c26c710c2485fca64c1b6669faf7756
Liudmyla Cheredniuk <Sapphire777.astro@gmail.com>
2026-03-19 08:27:57 +0300

    add tasks for homework_4

---
+++ jsBasics/task1.js
@@ -0,0 +1,13 @@
+let text = "hello";
+let num = 5;
+let flag = true;
+let nothing = null;
+let x;
+
+// перевірка змінних
+
+console.log(text);
+console.log(num);
+console.log(flag);
+console.log(nothing);
+console.log(x);
