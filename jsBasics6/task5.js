6e44e16c6be6b2c3f9f7ed93d9a54a4c38d6a84c
Liudmyla Cheredniuk <Sapphire777.astro@gmail.com>
2026-04-07 08:28:48 +0300

    fix: виправила завдання 5

--- jsBasics6/task5.js
+++ jsBasics6/task5.js
@@ -1,13 +1,27 @@
 const users = [
-  { name: "Людмила", email: "liudmila@gmail.com", age: 29 },
-  { name: "Андрій", email: "andriy@gmail.com", age: 30 },
-  { name: "Олег", email: "oleg@gmail.com", age: 45 }
+  { email: "liudmila@gmail.com", age: 29, isActive: true },
+  { name: "Андрій", city: "Київ", isActive: false },
+  { name: "Олег", email: "oleg@gmail.com" },
+  { age: 45, isActive: true }
 ];
 
+for (const {
+  name = "Невідомо",
+  email = "Не вказано",
+  age = "Не вказано",
+  isActive
+} of users) {
 
-for (const { name, email, age } of users) {
+  const activeStatus =
+    isActive === undefined
+      ? "Не вказано"
+      : isActive
+      ? "Так"
+      : "Ні";
+
   console.log("Ім'я:", name);
   console.log("Email:", email);
   console.log("Вік:", age);
+  console.log("Активний:", activeStatus);
   console.log("-----");
 }
